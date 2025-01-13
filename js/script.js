562102f03a4a83faca85cbb559afb4865e4c3823
yasin <yasinbesni@gmail.com>
2025-01-13 12:27:02 +0300

    added animation for sidebar and hamburger(button)

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector('.hamburger');
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+const mobilnav = document.querySelector(".mobilnav")
+
+
+
+hamburger.addEventListener("click",()=>{
+    bar1.classList.toggle("animationBar1")
+    bar2.classList.toggle("animationBar2")
+    bar3.classList.toggle("animationBar3")
+    mobilnav.classList.toggle("sidebar")
+    hamburger.classList.toggle("hamburgerFixed")
+})
